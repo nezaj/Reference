5e86f97b453b2d4c75b6827dd2d7a5aedcd25f69
Joe Averbukh <joeaverbukh@gmail.com>
2014-05-04 19:55:28 -0700

    Improve bisection

--- think_complexity/analysis/bisection.py
+++ think_complexity/analysis/bisection.py
@@ -1,31 +1,19 @@
 
 def bisection(l, t, shift=0):
     """
-    Uses binary search to find index of target in a list. Assumes list is sorted
-
-    Algorithm:
-    If the list is empty than the target cannot be located. Return None.
-
-    For a non-empty list, check whether the middle element is the target.
-    If it is, return the middle index plus all previously discarded elements.
-    If it isn't and the middle index is 0 than the target does not exist.
-    Return None.
-
-    If the target is smaller than the middle element, search the left half of the
-    list. If the target is larger than the middle element, search the right half
-    of the list. When searching the right half, we must keep track of all the
-    discarded elements to the left.
+    Uses binary search to find index of target in a list.
+    Note: Assumes list is already sorted
     """
-    if l == []:
-        return None
+    lo = 0
+    hi = len(l)
 
-    mid = len(l) / 2
-    if t == l[mid]:
-        return shift + mid
-    if mid == 0:
-        return None
+    while lo < hi:
+        mid = (lo + hi) / 2
+        if t == l[mid]:
+            return mid
+        elif t < l[mid]:
+            hi = mid
+        else:
+            lo = mid + 1
 
-    if t < l[mid]:
-        return bisection(l[:mid], t, shift)
-    else:
-        return bisection(l[mid:], t, shift + mid)
+    return None
